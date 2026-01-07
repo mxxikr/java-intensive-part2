eda8168f8c7e5cdb4cfe25c09676613beedf2e0e
mxxikr <mxxikr@gmail.com>
2026-01-07 13:21:41 +0900

    클래스2: 보이지 않는 임시 객체

---
+++ src/class2/tmpObj/Main.java
@@ -0,0 +1,12 @@
+package class2.tmpObj;
+
+public class Main {
+    public static void main(String[] args) {
+        String left = "Hello";
+        String right = "World";
+
+        System.out.println((left + right).toUpperCase());
+        String result = left + right;
+        System.out.println(result);
+    }
+}
