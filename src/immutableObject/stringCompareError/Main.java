75db72991f05eacfee39beecf52ee1cb95561943
mxxikr <mxxikr@gmail.com>
2026-01-08 15:39:11 +0900

    불변 객체와 String 클래스: 문자열 비교

---
+++ src/immutableObject/stringCompareError/Main.java
@@ -0,0 +1,17 @@
+package immutableObject.stringCompareError;
+
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+        System.out.println("");
+
+        String s3 = new String("World");
+        String s4 = new String("World");
+        System.out.println(s3 == s4);
+        System.out.println(s3.equals(s4));
+        System.out.println(s3.compareTo(s4) == 0);
+    }
+}
