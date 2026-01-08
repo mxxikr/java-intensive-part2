cc26e47cd1e0e9782d58033cdb25471b8994920a
mxxikr <mxxikr@gmail.com>
2026-01-08 15:54:43 +0900

    불변 객체와 String 클래스: 래퍼 클래스, 박싱과 언박싱

---
+++ src/immutableObject/wrapperClass/Main.java
@@ -0,0 +1,17 @@
+package immutableObject.wrapperClass;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer intObj = 128;
+        Integer intObj2 = (int)128;
+        int tmp = intObj;
+
+        System.out.printf("%d\n", 128);
+        System.out.println(String.valueOf(128));
+        System.out.println(intObj.toString());
+        System.out.println(Integer.valueOf("256"));
+
+        System.out.println(128 == 128);
+        System.out.println(intObj == intObj2);
+    }
+}
