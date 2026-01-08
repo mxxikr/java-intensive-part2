21445246118e30153798fe07a47cb0e23ea43078
mxxikr <mxxikr@gmail.com>
2026-01-08 15:50:41 +0900

    불변 객체와 String 클래스: JVM이 문자열 상수를 관리하는 구조

---
+++ src/immutableObject/internAndPool/Main.java
@@ -0,0 +1,15 @@
+package immutableObject.internAndPool;
+
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+        System.out.println(s1 == s2);
+
+        String s3 = new String("World");
+        String s4 = s3.intern();
+        System.out.println(s3 == s4);
+        System.out.println("World" == s3);
+        System.out.println("World" == s4);
+    }
+}
