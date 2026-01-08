fc634a0e93124b54b6692b9f3891fe64262166c7
mxxikr <mxxikr@gmail.com>
2026-01-08 20:33:55 +0900

    내부 클래스와 익명 객체: 패키지

---
+++ src/innerClassAndAnonymous/packageSample/Main.java
@@ -0,0 +1,8 @@
+package innerClassAndAnonymous.packageSample;
+
+public class Main {
+    public static void main(String[] args) {
+        UserData user = new UserData("Hosung");
+        System.out.println(user.getName());
+    }
+}
---
+++ src/innerClassAndAnonymous/packageSample/UserData.java
@@ -0,0 +1,12 @@
+package innerClassAndAnonymous.packageSample;
+
+public class UserData {
+    public UserData(String name) {
+        this.name = name;
+    }
+
+    private String name;
+    public String getName() {
+        return name;
+    }
+}
